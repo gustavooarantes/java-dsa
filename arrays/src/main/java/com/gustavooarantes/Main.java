a8895a8edf03b4e55fd0a99fee2d0511151fbe16
gustavooarantes <profissional.gustavoa@gmail.com>
2025-09-19 03:39:46 -0300

    .

---
+++ arrays/src/main/java/com/gustavooarantes/Main.java
@@ -0,0 +1,16 @@
+package com.gustavooarantes;
+
+public class Main {
+
+	/**
+	 * Arrays store a specific data type in a contiguous memory space that can not
+	 * be changed.
+	 * Each element has its memory space and a unique index used for accessing it.
+	 *
+	 * Arrays can be one dimensional or multidimensional (often called matrices).
+	 */
+
+	public static void main(String[] args) {
+
+	}
+}
